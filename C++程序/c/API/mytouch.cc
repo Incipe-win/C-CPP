92715f3692618e35e0a2729e75fa08b289744cfe
huchaowu <whc_9_13@163.com>
2020-05-12 12:20:45 +0800

    update

---
+++ C++程序/c/API/mytouch.cc
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("./a.out filename\n");
+    return -1;
+  }
+  int fd = open(argv[1], O_RDONLY | O_CREAT, 0666);
+  close(fd);
+  return 0;
+}
---
+++ C++程序/c/makefile/case2/Include/head.h
@@ -0,0 +1,8 @@
+#ifndef HEAD_H_
+#define HEAD_H_
+#include <iostream>
+
+int add(int x, int y);
+int sub(int x, int y);
+
+#endif  // HEAD_H_
