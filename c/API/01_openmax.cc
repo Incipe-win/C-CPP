f9ab306ea8830252a6823aae9136ddf2028cd943
Incipe-win <whc_9_13@163.com>
2020-05-12 21:31:58 +0800

    update

--- c/API/01_openmax.cc
+++ c/API/01_openmax.cc
@@ -9,7 +9,7 @@ int main() {
   char filename[128] = {0};
   while (1) {
     sprintf(filename, "temp_%4d", num++);
-    int fd = open(filename, O_CREATE | O_RDONLY, 0666);
+    int fd = open(filename, O_CREAT | O_RDONLY, 0666);
     if (fd < 0) {
       perror("open err ");
       // close(fd);
